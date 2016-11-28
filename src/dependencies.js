12a1fa5443f858d5661ed26b4123e929388e9803
Viktor Zhakhalov <zhakhalov@gmail.com>
2016-11-28 23:12:02 +0200

    add es6-shim

--- src/dependencies.js
+++ src/dependencies.js
@@ -1,14 +1,17 @@
 /**
  * Third Party dependencies goes here
  */
+
+require('es6-shim/es6-shim.js');
+require('axios');
+require('lodash');
+
+require('reflect-metadata');
+require('zone.js');
+
 require('@angular/common');
 require('@angular/platform-browser-dynamic');
 require('@angular/forms');
 require('@angular/core');
-
-require('reflect-metadata');
-require('zone.js');
 require('ionic-angular');
 require('ionic-native');
-require('axios');
-require('lodash');
